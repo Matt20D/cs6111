c03122b17ec060af0c07d98a414acff58cf28885
Matthew Duran <mduran2429@gmail.com>
2022-02-20 13:28:34 -0500

    final change to test.sh

--- proj1/test.sh
+++ proj1/test.sh
@@ -1,7 +1,8 @@
 #python3 google-query.py AIzaSyANgIgrPnITWd3HBYXVJpV_WM3mVQd8pME 7c642eecbff553d82 1 "Columbia University"
 # python3 google-query.py AIzaSyANgIgrPnITWd3HBYXVJpV_WM3mVQd8pME 7c642eecbff553d82 1 "York New Mets"
 #python3 google-query.py AIzaSyANgIgrPnITWd3HBYXVJpV_WM3mVQd8pME 7c642eecbff553d82 1 "jaguar"
-python3 google-query.py AIzaSyANgIgrPnITWd3HBYXVJpV_WM3mVQd8pME 7c642eecbff553d82 1 "per se"
-# python3 google-query.py AIzaSyANgIgrPnITWd3HBYXVJpV_WM3mVQd8pME 7c642eecbff553d82 1 "cases"
+#python3 google-query.py AIzaSyANgIgrPnITWd3HBYXVJpV_WM3mVQd8pME 7c642eecbff553d82 0.9 "per se"
+#python3 google-query.py AIzaSyANgIgrPnITWd3HBYXVJpV_WM3mVQd8pME 7c642eecbff553d82 0.9 "cases"
 # python3 google-query.py AIzaSyANgIgrPnITWd3HBYXVJpV_WM3mVQd8pME 7c642eecbff553d82 1 "tiger" # tiger woods
 # python3 google-query.py AIzaSyANgIgrPnITWd3HBYXVJpV_WM3mVQd8pME 7c642eecbff553d82 1 "to be or not to be"
+python3 google-query.py AIzaSyANgIgrPnITWd3HBYXVJpV_WM3mVQd8pME 7c642eecbff553d82 0.9 "brin"
