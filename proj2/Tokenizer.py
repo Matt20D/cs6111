cc2eccf8d345e36d53cd63a21ef4b6401c3c5cbb
Matt Duran <md3420@columbia.edu>
2022-03-22 13:40:09 +0000

    added filter for candidate relations
    Based on relation type we will filter out incorrectly extracted
    relations. This works, and produces a set of valid candidate tuples.
    
    Now I need to work it in conjuction with Bert inorder to predict
    a confidence level for the extracted tuple.

--- proj2/Tokenizer.py
+++ proj2/Tokenizer.py
@@ -31,6 +31,43 @@ RELATIONS = {
 		{"Subject": "ORGANIZATION", "Object": "PERSON"}
 }
 
+def is_valid_relation(rel_type: str, pair: dict) -> bool:
+
+	#
+	# extract types
+	#
+
+	# proj type
+	obj_type = pair['obj'][1]
+	# subject type
+	sub_type = pair['subj'][1]
+
+
+	#
+	# type check for a given relation
+	#
+	if rel_type == "Schools_Attended" or rel_type == "Work_For" or rel_type == "Top_Member_Employees":
+		#print(pair)
+		#print(rel_type)
+		#print(RELATIONS[rel_type])
+		if  obj_type == RELATIONS[rel_type]["Object"] and \
+			sub_type == RELATIONS[rel_type]["Subject"]:
+			return True
+		else:
+			return False
+	elif rel_type == "Live_In":
+		#print(pair)
+		#print(rel_type)
+		#print(RELATIONS[rel_type])
+		if  obj_type in RELATIONS[rel_type]["Object"] and \
+			sub_type == RELATIONS[rel_type]["Subject"]:
+			return True
+		else:
+			return False
+	# a little bit of defensive programming, we should only be getting tuples
+	# of the above 4 rel types
+	else:
+		return False
 class Tokenizer(object):
 	
 	def __init__(self, url, current_tuples, relation):
@@ -40,8 +77,7 @@ class Tokenizer(object):
 		self.relation_type = relation 
 
 	def execute_get_request(self) -> None:
-		print(self.relation_type)
-		quit()
+
 		# send get request for the webpage
 		try:
 			print("\tGetting the webpage using GET request") 
@@ -75,26 +111,51 @@ class Tokenizer(object):
 		print("\tAnnotating the webpage using spacy...")
 		nlp = spacy.load("en_core_web_lg")
 		doc = nlp(soup_text)
-		print("\tExtracted {} sentences.".format(len(doc)))
+		print("\tExtracted {} sentences.".format(len(doc.sents)))
 
 		entities_of_interest = ["ORGANIZATION", "PERSON", "LOCATION", "CITY", "STATE_OR_PROVINCE", "COUNTRY"]
 
 
-		# 1) use spaCY to ID sentences in web text along with the 
-		#    named entities (if any), that appear
-		# 2) Then, you should construct entity pairs
 		# 3) run the expensive SpanBERT model, separately only over each 
 		#    entity pair that contains both required named entities for the relation of interest
 		# 4) IMPORTANT: skip SpanBERT for any entity pairs that are missing one or two entities 
 		# of the type required by the relation.
 		for sentence in doc.sents:
+
+			# 1) use spaCY to ID sentences in web text along with the 
+			#    named entities (if any), that appear
 			print("\n\n\tProcessing sentence: {}".format(sentence))
 			print("\tTokenized sentence: {}".format([token.text for token in sentence]))
-			#print([(e.text, e.start_char, e.end_char, e.label_) for e in doc.ents])
 			ents = get_entities(sentence, entities_of_interest)
 			print("\tspaCy extracted entities: {}".format(ents))
 
+			# 2) Then, you should construct entity pairs
+			candidate_pairs = []
+			sentence_entity_pairs = create_entity_pairs(sentence, entities_of_interest)
+			
+			for ep in sentence_entity_pairs:
+				
+				# generate the pairs
+				pair1 = {"tokens": ep[0], "subj": ep[1], "obj": ep[2]}
+				pair2 = {"tokens": ep[0], "subj": ep[2], "obj": ep[1]}
 
+				# filter out only subject-object pairs of the right type for the target relation
+				pair1_valid = is_valid_relation(self.relation_type, pair1)
+				pair2_valid = is_valid_relation(self.relation_type, pair2)
+
+				# if one of the pairs is true, then the other should be false
+				if pair1_valid == True:
+					# append valid pairs
+					candidate_pairs.append(pair1)  # e1=Subject, e2=Object
+					continue
+				elif pair2_valid == True:
+					# append valid pairs
+					candidate_pairs.append(pair2)  # e1=Object, e2=Subject
+					continue
+
+			print("\tGenerated Sentence entity pairs: {}, remaining valid pairs: {}".format(\
+									len(sentence_entity_pairs), len(candidate_pairs)))
+			quit()
         #print('working on new doc')
         #try:
         #    relations = extract_relations(doc, spanbert, entities_of_interest)
