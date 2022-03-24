b1f0fb788eec17e2234cf2dc6442ad69626dd9c3
Matt Duran <md3420@columbia.edu>
2022-03-24 14:38:04 +0000

    shell script to clean directory after done

---
+++ proj2/dir_cleanup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm LICENSE 
+rm README.md 
+rm download_finetuned.sh 
+rm example_relations.py 
+rm -rf pretrained_spanbert/ 
+rm -rf pytorch_pretrained_bert/ 
+rm -rf SpanBERT/ 
+rm relations.txt 
+rm requirements-spanBERT.txt 
+rm spacy_help_functions.py 
+rm spanbert.py
