d143c01f5d52f195c78b1a586548f4d8520cee2f
Matt Duran <md3420@columbia.edu>
2022-03-24 14:55:37 +0000

    fixed the setup and spanbert scripts
    setup will now create the venv according to my dependencies using
    my_requirements.txt. then it will call the get spacy and
    getSpanBert scripts
    
    spacy script is the same
    
    spanbert script will build everything normally, and then move all
    of the files back to where iterative-set-expansion and Tokenizer.py
    live.
    
    This was the fix I found in OH. move all the spanbert files out to
    where my source code is.

--- proj2/getSpanBert.sh
+++ proj2/getSpanBert.sh
@@ -2,3 +2,4 @@ git clone https://github.com/gkaramanolakis/SpanBERT
 cd SpanBERT
 pip3 install -r requirements.txt
 bash download_finetuned.sh
+mv ./* ..
--- proj2/setup.sh
+++ proj2/setup.sh
@@ -2,4 +2,6 @@
 
 python3 -m venv venv
 source venv/bin/activate
-pip3 install -r requirements.txt
+pip3 install -r my_requirements.txt
+./getSpacy.sh
+./getSpanBert.sh
